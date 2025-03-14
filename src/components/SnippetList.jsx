67fe1e0b34324428b9f763112dbc1115e69c76eb
Alyssa Ngo <amaengo@protonmail.com>
2025-03-14 19:01:17 +1000

    Added more syntax highlight styles that are selectable using a dropdown

--- src/components/SnippetList.jsx
+++ src/components/SnippetList.jsx
@@ -1,16 +1,15 @@
 import { Card, CardContent, Typography, Button } from "@mui/material";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 // Shows list of all code snippets and their details
-const SnippetList = ({ snippets, onDelete }) => {
+const SnippetList = ({ snippets, onDelete, theme }) => {
   return (
     <div>
       {snippets.map((snippet, index) => (
         <Card key={index} style={{ marginBottom: "10px" }}>
           <CardContent>
             <Typography variant="h6">{snippet.title} ({snippet.language})</Typography>
-            <SyntaxHighlighter language={snippet.language.toLowerCase() || "javascript"} style={atomDark}>
+            <SyntaxHighlighter language={snippet.language.toLowerCase() || "javascript"} style={theme}>
               {snippet.code}
             </SyntaxHighlighter>
             <Button onClick={() => onDelete(index)} variant="outlined" color="error">Delete</Button>
